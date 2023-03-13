a20713130e505b76934ae5224791b8b30ecb6d92
ValeriaOvod <krossovki.ukraine.vzutya@gmail.com>
2023-03-13 21:06:05 +0200

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import Filter from './Filter/Filter';
 import NavBar from '../components/NavBar/NavBar';
 import { selectIsLoading, selectError } from '../redux/selectors';
 import { useSelector } from 'react-redux';
 import UserRoutes from 'UserRoutes';
-import store from './redux/store';
+import { store, persistor } from '../redux/store';
 
 const App = () => {
   // const isLoading = useSelector(selectIsLoading);
@@ -15,16 +16,18 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <NavBar />
-        <UserRoutes />
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h1>Contacts</h1>
-        <Filter />
-        {/* {isLoading && !error && <b>Request in progress...</b>} */}
-        <ContactList />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <NavBar />
+          <UserRoutes />
+          <h1>Phonebook</h1>
+          <ContactForm />
+          <h1>Contacts</h1>
+          <Filter />
+          {/* {isLoading && !error && <b>Request in progress...</b>} */}
+          <ContactList />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,15 +1,27 @@
 import { combineReducers } from "@reduxjs/toolkit";
-
+import { persistReducer } from 'redux-persist';
 import authReducer from "./auth/auth-slice";
-import { contactsReducer } from './sliceContact'
-import { filterReducer } from './filterSlice'
+
+import { contactsReducer } from '../redux/sliceContact'
+import { filterReducer } from '../redux/filterSlice'
+import storage from 'redux-persist/lib/storage';
 
 
-export const rootReducer  = combineReducers({
+
+const persistConfig = {
+  key: 'root',
+    storage,
+    whitelist: ['token']
+}
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+ const rootReducer  = combineReducers({
   reducer: {
-    auth:authReducer,
+    auth:persistedAuthReducer,
     contacts: contactsReducer,
       filter: filterReducer,
   },
  
-    });
+ });
+    
+export default rootReducer;
