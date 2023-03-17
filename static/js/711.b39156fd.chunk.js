ecce4e9567656555226ef92da18e8f8685187fa1
ValeriaNikitiuk <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-03-17 22:34:10 +0000

    Deploying to gh-pages from @ ValeriaNikitiuk/goit-react-hw-08-phonebook@831d354f8a11b7e9ff27095274afc2efe896f3fa 🚀

---
+++ static/js/711.b39156fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[711],{711:function(e,o,n){n.r(o),n.d(o,{default:function(){return c}});n(791);var t=n(839),r=n(184);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{className:t.Z.text,children:"Hi friend,Welcome to your phonebook!"})})}}}]);
+//# sourceMappingURL=711.b39156fd.chunk.js.map
